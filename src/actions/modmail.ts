53ba069e1984115e0ac9008715587225ed6d7920
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2025-05-05 14:02:24 +0100

    Add option to stop sending modmails after N approvals #33

--- src/actions/modmail.ts
+++ src/actions/modmail.ts
@@ -1,9 +1,30 @@
 import { Comment, Post, SettingsValues, TriggerContext } from "@devvit/public-api";
 import { ProblematicSubsResult } from "../getProblematicItems.js";
 import { AppSetting } from "../settings.js";
+import { APPROVALS_KEY } from "../handleContentCreation.js";
 
 export async function sendModmail (target: Post | Comment, problematicItemsResult: ProblematicSubsResult, settings: SettingsValues, context: TriggerContext) {
     if (!settings[AppSetting.ModmailEnabled]) {
+        return;
+    }
+
+    const allowlistThreshold = settings[AppSetting.ModmailNumber] as number | undefined ?? 3;
+    let shouldModmail = true;
+    let currentApprovalCount: number | undefined;
+    if (allowlistThreshold) {
+        try {
+            currentApprovalCount = await context.redis.zScore(APPROVALS_KEY, target.authorName);
+            if (currentApprovalCount !== undefined && currentApprovalCount >= allowlistThreshold) {
+                console.log(`User ${target.authorName} has too many approvals to report.`);
+                shouldModmail = false;
+            }
+        } catch {
+            // User has no approvals, so always modmail.
+        }
+    }
+
+    if (!shouldModmail) {
+        console.log(`User ${target.authorName} has too many approvals to modmail.`);
         return;
     }
 
